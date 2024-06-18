d087df7ee64bf7de46eedf3b872af926c91a38d2
KevinRawal <kevinrawal30@gmail.com>
2024-06-18 07:57:16 +0530

    added get context method to generate context from given html

--- services/service.py
+++ services/service.py
@@ -1,4 +1,12 @@
 from transformers import pipeline
+from bs4 import BeautifulSoup
+
+# we are assuming that the html is given with the request. 
+def get_context_from_html(html):
+    "create a context for product using given html"
+    # TODO - we should use llm to create context from html
+    # TODO - llm output will be key value pair, which used to give user's query answer.
+    pass
 
 def get_answer_from_model(context, question):
     """Gets an answer from a pre-trained model based on the input context and question."""
